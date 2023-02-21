ad5711eccff54815ce2cb4e9fb55958717522e0c
Forslunddavid <forslund.david@gmail.com>
2023-02-21 22:25:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Hämta ur DOM
+const pokedexButton = document.querySelector('#pokedex-button');
+const homeButton = document.querySelector('#home-button');
+const myTeamButton = document.querySelector('#my-team-button');
+
+//API
+const pokeApi = 'https://pokeapi.co'
+
+// Event Listeners
+pokedexButton.addEventListener('click', async () => {
+
+})
+
+homeButton.addEventListener('click', async () => {
+
+})
+
+myTeamButton.addEventListener('click', async () => {
+
+})
+
