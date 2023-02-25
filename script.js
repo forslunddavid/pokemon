583465cfbb46e907b848e7845053aa8d8b1e935d
Forslunddavid <forslund.david@gmail.com>
2023-02-25 18:43:14 +0100

    css bråkat med lägga till i lag hela dagen

--- script.js
+++ script.js
@@ -21,6 +21,21 @@ const searchForm = document.getElementById("search-form");
 
 const searchResults = document.getElementById("search-results");
 
+const searchButton = document.querySelector('#search-button');
+
+const searchBar = document.querySelector('#search-bar');
+
+const searchResultsContainer = document.getElementById("search-results-container");
+
+//My Team konstanter
+const startingTeam = document.querySelector('.starting-team');
+const reserves = document.querySelector('.reserves');
+
+const startingTeamContainer = document.querySelector(".starting-team");
+const reserveTeamContainer = document.querySelector(".reserve-team");
+
+const pokemonData = {};
+
 
 // Eventlisteners knappar
 
@@ -126,5 +141,101 @@ searchInput.addEventListener("keyup", function() {
         searchResults.innerHTML = "";
     }
 });
+
+// Function to add a pokemon to the starting team or reserves
+function addPokemonToTeam(pokemonName) {
+  // Create a new div to hold the pokemon
+	const newPokemonDiv = document.createElement('div');
+	newPokemonDiv.innerText = pokemonName;
+
+  // Check if there is room in the starting team
+	if (startingTeam.children.length < 3) {
+    startingTeam.appendChild(newPokemonDiv);
+	} else {
+    reserves.appendChild(newPokemonDiv);
+	}
+}
+
+// Function to display search results
+function displaySearchResults(results) {
+  // Clear previous results
+	searchResults.innerHTML = '';
+
+  // Loop through results and create a button for each pokemon
+	results.forEach(result => {
+    const pokemonName = result.name;
+    const newButton = document.createElement('button');
+    newButton.innerText = pokemonName;
+    newButton.addEventListener('click', () => {
+      // Prompt the user for a name to use for the pokemon
+    	const newName = prompt('Enter a name for this pokemon:');
+    	if (newName) {
+        pokemonData[newName] = result;
+        addPokemonToTeam(newName);
+    	}
+    });
+    searchResults.appendChild(newButton);
+	});
+}
+
+// Function to search for pokemon
+async function searchPokemon() {
+	const searchTerm = searchBar.value.toLowerCase();
+	if (pokemonData[searchTerm]) {
+    addPokemonToTeam(searchTerm);
+	} else {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm}`);
+    if (response.ok) {
+    	const data = await response.json();
+    	pokemonData[searchTerm] = data;
+    	addPokemonToTeam(searchTerm);
+    } else {
+    	alert(`No pokemon found with name '${searchTerm}'`);
+    }
+	}
+	searchBar.value = '';
+searchResults.innerHTML = '';
+}
+
+searchButton.addEventListener('click', searchPokemon);
+
+function createPokemonCard(pokemon) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+
+  const cardTitle = document.createElement("h5");
+  cardTitle.classList.add("card-title");
+  cardTitle.textContent = pokemon.name;
+
+  const addButton = document.createElement("button");
+  addButton.classList.add("btn", "btn-primary");
+  addButton.textContent = "Add to Team";
+  addButton.addEventListener("click", () => {
+    const pokemonName = prompt("Enter a name for your Pokemon:");
+    if (pokemonName) {
+      const teamCount = startingTeamContainer.childElementCount;
+      if (teamCount < 3) {
+        const pokemonCard = createPokemonCard(pokemon);
+        const nameElement = pokemonCard.querySelector(".card-title");
+        nameElement.textContent = `${pokemonName} (${pokemon.name})`;
+        startingTeamContainer.appendChild(pokemonCard);
+      } else {
+        const pokemonCard = createPokemonCard(pokemon);
+        const nameElement = pokemonCard.querySelector(".card-title");
+        nameElement.textContent = `${pokemonName} (${pokemon.name})`;
+        reserveTeamContainer.appendChild(pokemonCard);
+      }
+    }
+  });
+
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(addButton);
+  card.appendChild(cardBody);
+
+  return card;
+}
 
 //klicka på en pokemon i sökresultat lägga till den i my team samt välja namn. max 3 i my team, resten ska hamna i reserver
